46d7186d76f9d326710a520f6f7984b32f6ab937
Ahsan <sajibnet@gmail.com>
2024-09-18 21:44:24 +0300

    Add Part1 courseInfo array style

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,31 +1,37 @@
 const App = () => {
     const course = 'Half Stack application development'
-    
-    // Using objects for parts
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
-    }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
+  
+    // Array of objects representing parts of the course
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   
     return (
       <div>
         <h1>{course}</h1>
-        
-        {/* Accessing the properties of each part object */}
-        <p>{part1.name} {part1.exercises}</p>
-        <p>{part2.name} {part2.exercises}</p>
-        <p>{part3.name} {part3.exercises}</p>
   
-        {/* Summing the exercises */}
-        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+        {/* Mapping over the parts array to display each part */}
+        {parts.map((part, index) => (
+          <p key={index}>
+            {part.name} {part.exercises}
+          </p>
+        ))}
+  
+        {/* Calculating the total number of exercises */}
+        <p>
+          Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+        </p>
       </div>
     )
   }

a877f771590c9874be770fff14076b38eadb9f12
Ahsan <sajibnet@gmail.com>
2024-09-22 14:40:43 +0300

    Add Part1 courseInfo single object style

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,28 +1,31 @@
+
 const App = () => {
-    const course = 'Half Stack application development'
-  
-    // Array of objects representing parts of the course
-    const parts = [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+    // Single object containing the course name and parts
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
   
     return (
       <div>
-        <h1>{course}</h1>
+        {/* Accessing the course name */}
+        <h1>{course.name}</h1>
   
         {/* Mapping over the parts array to display each part */}
-        {parts.map((part, index) => (
+        {course.parts.map((part, index) => (
           <p key={index}>
             {part.name} {part.exercises}
           </p>
@@ -30,11 +33,11 @@ const App = () => {
   
         {/* Calculating the total number of exercises */}
         <p>
-          Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+          Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}
         </p>
       </div>
     )
   }
-  
+
   export default App
   
